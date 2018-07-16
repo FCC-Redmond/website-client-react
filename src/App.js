f2ce006877c7d39f8b67b45f4e32267bb6eebee5
Amber Kim <ambergkim@gmail.com>
2018-07-15 22:50:51 -0700

    fetch members

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import Welcome from './components/Welcome';
 import Members from './components/Members';
-import Projects from './components/Projects';
+// import Projects from './components/Projects';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
         <Navigation />
         <Welcome />
         <Members />
-        <Projects />
+        {/* <Projects /> */}
       </div>
     );
   }
--- src/components/Members.js
+++ src/components/Members.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-const renderMembers = () => {
-  let arr = [];
-  for (let i = 1; i < 8; i++) {
-    arr.push(
-      <Col sm={4} xs={12} className="member">
+class Members extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      members: []
+    }
+
+    this.renderMembers = this.renderMembers.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://fccredmond.herokuapp.com/api/v0/members')
+    .then(res => res.json())
+    .then(body => {
+      this.setState({members: body.data});
+    })
+  }
+
+  renderMembers() {
+    let memberJsx = this.state.members.map(member => {
+      return <Col sm={4} xs={12} className="member" key={member._id}>
         <div className="member-container">
           <div className="picture" />
-          <div className="information">
-            <div className="name">John Smith</div>
-            <div className="skills">HTML, CSS, Javascript</div>
+            <div className="information">
+            <div className="name">{member.firstName} {member.lastName}</div>
+            <div className="skills">{member.skills}</div>
             <div className="links" />
           </div>
         </div>
       </Col>
-    );
+    })
+    return memberJsx;
   }
-  return arr;
-};
 
-const Members = () => {
-  return (
-    <Grid className="grid-container" id="members">
+  render() {
+    return <Grid className="grid-container" id="members">
       <h1>MEMBERS</h1>
-      <Row className="grid-row">{renderMembers()}</Row>
+      <Row className="grid-row">{this.renderMembers()}</Row>
     </Grid>
-  );
-};
+  }
+}
 
 export default Members;
